06d184ad4884daa13f0a321e1f4ac2003697a6cf
Natalja Sniega <natalja.protasova@gmail.com>
2021-03-02 09:25:23 +0200

    my first commit

--- SEF - Participants Workspace/SEF/src/sef/module3/activity/VariableAssignmentActivity.java
+++ SEF - Participants Workspace/SEF/src/sef/module3/activity/VariableAssignmentActivity.java
@@ -15,8 +15,13 @@ public class VariableAssignmentActivity {
 	public static void main(String[] args) {
 		
 		// 1- Declare a variable of type int and assign it default value.
-		// 2- Update the value 
+		int myInteger = 5;
+
+		// 2- Update the value
+		myInteger = 6;
+
 		// 3- Print updated value to the console
+				System.out.println( "my new value is "+myInteger);
 
 	}
 
